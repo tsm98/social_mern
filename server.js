dcf6c4e2c08b761351b8b15cdbc2073679e5f163
Sairam Tushar Maddala <sairam.tm@gmail.com>
2020-04-18 02:42:12 +0530

    end of 2nd section

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
-const express = require("express");
-
+const express = require('express');
 const app = express();
 
-app.get("/", (req, res) => res.send("API chalu hai mama"));
+const connectDB = require('./config/db');
+
+connectDB();
+
+app.get('/', (req, res) => res.send('API chalu hai mama'));
+
+//Setting Routes
+app.use('/api/users', require('./routes/api/users'));
+app.use('/api/auth', require('./routes/api/auth'));
+app.use('/api/posts', require('./routes/api/posts'));
+app.use('/api/profile', require('./routes/api/profile'));
 
 const PORT = process.env.PORT || 5000;
 
